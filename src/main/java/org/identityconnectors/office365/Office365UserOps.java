81d47bc1f1e5f6e0b0e9970b79e4061e6f11bf53
Roman Pudil <roman.pudil@ami.cz>
2015-04-02 10:46:54 +0200

    Implemented: - connection test - fetch all accounts

--- src/main/java/org/identityconnectors/office365/Office365UserOps.java
+++ src/main/java/org/identityconnectors/office365/Office365UserOps.java
@@ -27,6 +27,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.common.security.GuardedString;
@@ -47,7 +49,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
- * 
+ *
  * @author Paul Heaney
  *
  */
@@ -55,9 +57,8 @@ public class Office365UserOps {
 
     private Office365Connector connector;
     private static final Log log = Log.getLog(Office365UserOps.class);
-
     private static final String NAME_ATTRIBUTE = "userPrincipalName";
-    
+
     public Office365UserOps(Office365Connector connector) {
         this.connector = connector;
     }
@@ -76,13 +77,13 @@ public class Office365UserOps {
             log.error("Name attribute is empty");
             throw new IllegalArgumentException("Name is mandatory on create events");
         }
-        
+
         log.ok("Name for create is {0}", name);
-        
-        if (this.connector.getConnection().isUserInAFederatedDomain(name.getNameValue()) 
+
+        if (this.connector.getConnection().isUserInAFederatedDomain(name.getNameValue())
                 && (AttributeUtil.toMap(createAttributes).get(Office365Connector.IMMUTABLEID_ATTR) == null)) {
             log.error("User is in a federated domain, though no immutableID has been passed, this is required for a Federated User");
-            throw new IllegalArgumentException("User ("+name.getNameValue()+") is in a federated domain, though no immutableID has been passed, this is required for a Federated User");
+            throw new IllegalArgumentException("User (" + name.getNameValue() + ") is in a federated domain, though no immutableID has been passed, this is required for a Federated User");
         }
 
         JSONObject jsonCreate = new JSONObject();
@@ -92,8 +93,8 @@ public class Office365UserOps {
 
         String license = null;
         boolean usageLocationSet = false;
-        
-        for (Attribute attr : createAttributes ) {
+
+        for (Attribute attr : createAttributes) {
             String attrName = attr.getName();
 
             Object value = null;
@@ -123,7 +124,7 @@ public class Office365UserOps {
                 } else {
                     value = AttributeUtil.getSingleValue(attr);
                 }
-            } 
+            }
 
             if (value != null) {
                 log.info("Adding attribute {0} with value {1}", attrName, value);
@@ -149,7 +150,7 @@ public class Office365UserOps {
                 pwd.put("password", password);
                 pwd.put("forceChangePasswordNextLogin", forceChangePasswordNextLogin);
                 jsonCreate.put("passwordProfile", pwd);
-            }catch (JSONException je){
+            } catch (JSONException je) {
                 log.error(je, "Error adding password to JSON attribute");
             }
         }
@@ -157,16 +158,16 @@ public class Office365UserOps {
         log.info("About to create account using JSON {0}", jsonCreate.toString());
 
         try {
-            uid = connector.getConnection().postRequest("/users?api-version="+Office365Connection.API_VERSION, jsonCreate);
+            uid = connector.getConnection().postRequest("/users?api-version=" + Office365Connection.API_VERSION, jsonCreate);
         } catch (ConnectorException ce) {
             log.error(ce, "Error creating user {0}", name);
         }
 
         log.ok("Created account {0} successfully", name);
-        
+
         if (uid != null && license != null) {
             log.info("Licenses to apply to newly created account");
-            
+
             if (usageLocationSet) {
                 log.info("Usage location was set so we can assign license");
                 boolean b = assignLicense(uid, license);
@@ -182,7 +183,6 @@ public class Office365UserOps {
 
         return uid;
     }
-
 
     public Uid updateUser(Uid uid, Set<Attribute> replaceAttributes, OperationOptions options) {
 
@@ -206,7 +206,7 @@ public class Office365UserOps {
 
         String password = null;
         Boolean forceChangePasswordNextLogin = new Boolean(false);
-        
+
         String license = null;
 
         for (Attribute attr : replaceAttributes) {
@@ -233,7 +233,7 @@ public class Office365UserOps {
                 } else {
                     value = AttributeUtil.getSingleValue(attr);
                 }
-            } 
+            }
 
             log.info("Replacing attribute {0} with value {1}", attrName, value);
             try {
@@ -258,7 +258,7 @@ public class Office365UserOps {
                 pwd.put("password", password);
                 pwd.put("forceChangePasswordNextLogin", forceChangePasswordNextLogin);
                 jsonModify.put("passwordProfile", pwd);
-            }catch (JSONException je){
+            } catch (JSONException je) {
                 log.error(je, "Error adding password to JSON attribute");
             }
         }
@@ -267,14 +267,14 @@ public class Office365UserOps {
 
         boolean b = false;
         try {
-            b = this.connector.getConnection().patchObject("/users/"+uid.getUidValue()+"?api-version="+Office365Connection.API_VERSION, jsonModify);
+            b = this.connector.getConnection().patchObject("/users/" + uid.getUidValue() + "?api-version=" + Office365Connection.API_VERSION, jsonModify);
         } catch (ConnectorException ce) {
             log.error(ce, "Error modifying user {0}", uid.getUidValue());
         }
 
         if (b) {
             log.ok("Modified account {0} successfully", uid.getUidValue());
-            
+
             if (license != null) {
                 log.info("Attempting to set the license");
                 b = assignLicense(uid, license);
@@ -290,10 +290,10 @@ public class Office365UserOps {
 
         return uid;
     }
-    
+
     public void deleteUser(final Uid uid) {
 
-        log.info ("In deleteUser");
+        log.info("In deleteUser");
 
         if (uid == null || (uid.getUidValue() == null)) {
             log.error("No UID specified for update");
@@ -302,7 +302,7 @@ public class Office365UserOps {
 
         log.ok("UID of {0} is present", uid.getUidValue());
 
-        boolean b = this.connector.getConnection().deleteRequest("/users/"+uid.getUidValue()+"?api-version="+Office365Connection.API_VERSION);
+        boolean b = this.connector.getConnection().deleteRequest("/users/" + uid.getUidValue() + "?api-version=" + Office365Connection.API_VERSION);
 
         if (b) {
             log.info("Sucessfully deleted account {0}", uid.getUidValue());
@@ -310,52 +310,61 @@ public class Office365UserOps {
             log.info("Failed to deleted account {0}", uid.getUidValue());
         }
     }
-    
+
     public void queryUser(String query, ResultsHandler resultsHandler, OperationOptions options) {
         log.info("queryUser");
-        
+
         if (query == null) {
             // retrieve all
-            log.error("Retreive all not implemented yet"); // TODO
-            this.connector.getConnection().getRequest("/users?api-version="+Office365Connection.API_VERSION);
+            log.info("Fetching All Users from Office 365");
+            JSONArray allObj;
+            try {
+                allObj = this.connector.getConnection().getRequest("/users?api-version=" + Office365Connection.API_VERSION).getJSONArray("value");
+                for (int i = 0; i < allObj.length(); i++) {
+                    ConnectorObject co = makeConnectorObject((JSONObject) allObj.get(i));
+                    if (co != null) {
+                        resultsHandler.handle(co);
+                    }
+                }
+            } catch (JSONException ex) {
+                Logger.getLogger(Office365UserOps.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         } else {
             log.info("Fetching Office 365 user {0}", query);
-            JSONObject obj = this.connector.getConnection().getRequest("/users/"+query+"/?api-version="+Office365Connection.API_VERSION);
-            
-            log.info("Retrieved {0} from Office 365", obj.toString());
-            
+            JSONObject obj = this.connector.getConnection().getRequest("/users/" + query + "/?api-version=" + Office365Connection.API_VERSION);
             ConnectorObject co = makeConnectorObject(obj);
-            
+
             if (co != null) {
                 resultsHandler.handle(co);
             }
         }
     }
-    
+
     public boolean assignLicense(Uid uid, String license) {
         log.info("assignLicense");
-        
+
         if (uid == null) {
             log.error("No UID specified on assignLicense");
             throw new IllegalArgumentException("No UID specified for assignLicense");
         }
-        
+
         log.ok("UID of {0} is present", uid.getUidValue());
-        
+
         if (license == null || license.length() == 0) {
             log.error("No license passed to assignLicense for {0}", uid.getUidValue());
-            throw new IllegalArgumentException("No license passed to assignLicense for "+uid.getUidValue());
+            throw new IllegalArgumentException("No license passed to assignLicense for " + uid.getUidValue());
         }
-        
+
         log.ok("License of {0} received for uid {1}", license, uid.getUidValue());
-        
+
         try {
             JSONObject lic = convertLicenseToJson(license);
-            
+
             log.info("Attempting license assignment with {0}", lic.toString());
 
-            Uid returnedUid = this.connector.getConnection().postRequest("/users/"+uid.getUidValue()+"/assignLicense?api-version="+Office365Connection.API_VERSION, lic);
-            
+            Uid returnedUid = this.connector.getConnection().postRequest("/users/" + uid.getUidValue() + "/assignLicense?api-version=" + Office365Connection.API_VERSION, lic);
+
             if (returnedUid != null && returnedUid.equals(Office365Connection.SUCCESS_UID)) {
                 log.info("License assigned successfully to {0}", uid.getUidValue());
                 return true;
@@ -363,45 +372,45 @@ public class Office365UserOps {
                 log.error("Failed to assign license");
                 return false;
             }
-        
+
         } catch (JSONException je) {
             log.error(je, "Error converting license {0} to JSON for {1}", license, uid.getUidValue());
-            throw new ConnectorException("Error converting license "+license+" to JSON for "+uid.getUidValue(), je);
+            throw new ConnectorException("Error converting license " + license + " to JSON for " + uid.getUidValue(), je);
         }
     }
 
     private ConnectorObject makeConnectorObject(JSONObject jsonObject) {
         log.info("makeConnectorObject");
-        
+
         if (jsonObject == null) {
             log.error("Passed empty jsonObject");
             return null;
         }
-        
+
         try {
             String objectType = jsonObject.getString("objectType");
             if (!objectType.equals("User")) {
                 log.error("Received object type {0} when doing a user query which is not supported", objectType);
-                throw new IllegalArgumentException("Received "+objectType+" when searching for a user, this should be User");
+                throw new IllegalArgumentException("Received " + objectType + " when searching for a user, this should be User");
             }
-            
+
             ConnectorObjectBuilder cob = new ConnectorObjectBuilder();
-            
+
             Uid uid = new Uid(jsonObject.getString("objectId"));
             String userPrincipalName = jsonObject.getString(NAME_ATTRIBUTE);
             cob.setUid(uid);
             cob.setName(userPrincipalName);
-            
+
             String[] attrs = {"accountEnabled", "city", "country", "department", "displayName", "facsimileTelephoneNumber", "givenName",
-                                "jobTitle", "mail", "mailNickname", "mobile", "otherMails", "physicalDeliveryOfficeName", "postalCode", 
-                                "preferredLanguage", "proxyAddresses", "state", "streetAddress", "surname", "telephoneNumber", 
-                                "usageLocation"};
-            
+                "jobTitle", "mail", "mailNickname", "mobile", "otherMails", "physicalDeliveryOfficeName", "postalCode",
+                "preferredLanguage", "proxyAddresses", "state", "streetAddress", "surname", "telephoneNumber",
+                "usageLocation"};
+
             for (String a : attrs) {
                 if (jsonObject.has(a)) {
                     Object value = jsonObject.get(a);
-                    log.info("Retreieved attribute {0} with value {1}", a, value);
-                    if (value != null  && value != JSONObject.NULL) {
+                    // log.info("Retreieved attribute {0} with value {1}", a, value);
+                    if (value != null && value != JSONObject.NULL) {
                         if (value instanceof JSONArray) {
                             JSONArray j = (JSONArray) value;
                             int length = j.length();
@@ -418,17 +427,16 @@ public class Office365UserOps {
                     log.info("No value returned for {0}", a);
                 }
             }
-            
+
             log.info("Object has the UID {0} and name {1}", uid, userPrincipalName);
-            
+
             return cob.build();
         } catch (JSONException je) {
             log.error(je, "Exception thrown parisng returned JSON on user query");
             return null;
         }
     }
-    
-    
+
     public JSONObject convertLicenseToJson(String license) throws JSONException {
         // INPUT  licensename:planname:planname:...
         log.info("convertLicenseToJson");
@@ -436,25 +444,25 @@ public class Office365UserOps {
         if (license != null && license.length() > 0) {
             log.info("Licnese string passed");
             String[] components = license.split(":");
-            
+
             /*
-                String object = "{\"addLicenses\": [
-                                { \"disabledPlans\": [\"SHAREPOINTWAC_EDU\" , \"SHAREPOINTSTANDARD_EDU\" ], 
-                                \"skuId\": \"314c4481-f395-4525-be8b-2ec4bb1e9d91\" }
-                                ], 
-                \"removeLicenses\": null }";
-            */
-            
+             String object = "{\"addLicenses\": [
+             { \"disabledPlans\": [\"SHAREPOINTWAC_EDU\" , \"SHAREPOINTSTANDARD_EDU\" ], 
+             \"skuId\": \"314c4481-f395-4525-be8b-2ec4bb1e9d91\" }
+             ], 
+             \"removeLicenses\": null }";
+             */
+
             JSONObject obj = new JSONObject();
             JSONArray addObj = new JSONArray();
-            
+
             String skuId = connector.getConnection().getLicensePlanId(components[0]);
             JSONObject licenseObj = new JSONObject();
             if (skuId != null) {
                 log.info("valid license SKU of {0} passed", skuId);
-                
+
                 licenseObj.put("skuId", skuId);
-                
+
                 if (components.length == 1) {
                     log.info("Only a license sku passed, no plans - all assumed");
                     // we have just a single sku with no specific plans
@@ -463,21 +471,21 @@ public class Office365UserOps {
                     log.info("Plans passed with license");
                     // Need to do the inverse here and get the disables
                     Office365License lic = connector.getConnection().getLicensePlan(components[0]);
-                    
+
                     ArrayList<String> assignedPlans = new ArrayList<String>();
                     for (int i = 1; i < components.length; i++) {
                         assignedPlans.add(components[i]);
                     }
-                    
+
                     if (lic != null) {
                         log.info("Got valid license object for id {0}", components[0]);
                         Iterator<Office365ServicePlan> it = lic.getServicePlans().iterator();
                         ArrayList<String> unwantedPlans = new ArrayList<String>();
-                        
+
                         while (it.hasNext()) {
                             Office365ServicePlan sp = it.next();
                             log.info("Service plan on license {0}", sp.getServicePlanName());
-                            
+
                             if (!assignedPlans.contains(sp.getServicePlanName())) {
                                 log.info("Adding {0} to list of plans we don't want", sp.getServicePlanName());
                                 // We don't want this plan
@@ -487,16 +495,16 @@ public class Office365UserOps {
                                 }
                             }
                         }
-                        
+
                         licenseObj.put("disabledPlans", unwantedPlans);
                     }
-                    
+
                     addObj.put(licenseObj);
                 }
 
                 obj.append("addLicenses", licenseObj);
                 obj.put("removeLicenses", JSONObject.NULL); // TODO something smarter
-                
+
                 return obj;
             } else {
                 log.error("Invalid SKU/License passed {0}", components[0]);
@@ -507,17 +515,15 @@ public class Office365UserOps {
             return null;
         }
     }
-    
+
     /**
-     * 
-     * @param password
-     *            The password to format
+     *
+     * @param password The password to format
      * @return String the plain text version of the password
      */
     private String returnPassword(GuardedString password) {
         final String[] clearText = new String[1];
         GuardedString.Accessor accessor = new GuardedString.Accessor() {
-
             @Override
             public void access(char[] clearChars) {
                 clearText[0] = new String(clearChars);
